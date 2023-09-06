ee8d19ece46c843c28434f23e0fe860992de781e
WangPinChen <r17369@hotmail.com>
2023-09-06 17:38:12 +0800

    add models

---
+++ models/drinks.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+const drinkSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  image: {
+    type: String,
+  },
+  desc: {
+    type: Text,
+  },
+  isPopular: {
+    type: Boolean,
+    default: false
+  },
+  createdBy: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    index: true,
+    required: true
+  }
+})
+
+module.exports = mongoose.model('Drink', drinkSchema)
---
+++ models/store.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+const sotreSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  address: {
+    type: String,
+    required: true
+  },
+  phone: {
+    type: String,
+    required: true
+  },
+  city_id: {
+    type: Schema.Types.ObjectId,
+    ref: 'City',
+    index: true,
+    required: true
+  },
+  createdBy: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    index: true,
+    required: true
+  }
+})
+
+module.exports = mongoose.model('Store', sotreSchema)
