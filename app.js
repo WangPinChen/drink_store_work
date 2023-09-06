6387ca2f6c88bdf7978e96007fa0de0662634378
WangPinChen <r17369@hotmail.com>
2023-09-06 17:21:59 +0800

    add ./config/mongoose.js

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 const express = require('express')
 const exphbs = require('express-handlebars')
 const PORT = 3000
 const router = require('./routes')
 const app = express()
+
+require('./config/mongoose')
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
