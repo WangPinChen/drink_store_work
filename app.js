80252f4d1fd1bf796d3b3294d539e419e7ba0c49
WangPinChen <r17369@hotmail.com>
2023-09-06 17:13:22 +0800

    add route folder and create index.js to render home page

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
+const PORT = 3000
+const router = require('./routes')
 const app = express()
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
+
+app.use(router)
+
+app.listen(PORT, () => {
+  console.log(`App is running on http://localhost:${PORT}`)
+})
