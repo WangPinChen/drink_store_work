13a73aea43c96d3d3aa9912491d784e449d9cdf3
WangPinChen <r17369@hotmail.com>
2023-09-07 20:53:42 +0800

    modify admin/drinks page

--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -5,11 +5,6 @@ const adminController = require('../../controllers/admin-controller')
 
 router.get('/login', adminController.loginPage)
 // router.post('/login', passport.authenticate('local', { failureRedirect: '/admin/login', failureFlash: true }), adminController.login)
-// router.post('/login',
-//   passport.authenticate('local', {
-//     successRedirect: '/admin/drinks',
-//     failureRedirect: '/admin/login'
-//   }))
 router.post('/login', passport.authenticate('local', { failureRedirect: '/admin/login' }), adminController.login)
 router.get('/drinks', adminController.getDrinks)
 
